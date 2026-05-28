44b82ff8f3fab52316f33cadf93f35ade552d95d
adindayuriafifah14 <adindaafifah14@gmail.com>
2026-05-28 11:52:07 +0700

    menambahkan cek jika antrian hanya satu elemen

--- tugas.cpp
+++ tugas.cpp
@@ -52,4 +52,14 @@ public:
                 REAR = REAR + 1;
         }
         queue_array[REAR] = num;
+
+        // cek jika antrian hanya memiliki satu elemen
+            if (FRONT == REAR)
+            {
+                FRONT = -1;
+                REAR = -1;
+            }
+            else
+            {
+
     }
