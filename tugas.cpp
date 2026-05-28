dfe026a5d71fb103aaeb3fc5d853137d85ff3816
adindayuriafifah14 <adindaafifah14@gmail.com>
2026-05-28 11:51:19 +0700

    menambahkan void remove

--- tugas.cpp
+++ tugas.cpp
@@ -44,4 +44,12 @@ public:
                 REAR = REAR + 1;
         }
         queue_array[REAR] = num;
+
+        // jika rear berada di posisi terakhir array, kembali ke awal array
+            if (REAR == max - 1)
+                REAR = 0;
+            else
+                REAR = REAR + 1;
         }
+        queue_array[REAR] = num;
+    }
